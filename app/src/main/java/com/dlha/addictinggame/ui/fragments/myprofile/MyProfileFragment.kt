e68481a1bd0edfc49b22ef91d7e12475d50887d0
Wymm <wymmndp@gmail.com>
2021-05-12 23:03:40 +0700

    update login/regiser/myprofile ui

---
+++ app/src/main/java/com/dlha/addictinggame/ui/fragments/myprofile/MyProfileFragment.kt
@@ -0,0 +1,32 @@
+package com.dlha.addictinggame.ui.fragments.myprofile
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dlha.addictinggame.R
+
+class MyProfileFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MyProfileFragment()
+    }
+
+    private lateinit var viewModel: MyProfileViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.my_profile_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MyProfileViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
