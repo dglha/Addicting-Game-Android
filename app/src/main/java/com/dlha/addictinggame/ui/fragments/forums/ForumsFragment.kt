6f2a67b140c142f7e2203240c5c3618bdd7ac805
Wymm <wymmndp@gmail.com>
2021-05-15 23:14:07 +0700

    Cart Fragment

--- app/src/main/java/com/dlha/addictinggame/ui/fragments/forums/ForumsFragment.kt
+++ app/src/main/java/com/dlha/addictinggame/ui/fragments/forums/ForumsFragment.kt
@@ -1,60 +1,36 @@
 package com.dlha.addictinggame.ui.fragments.forums
 
+import android.os.Build
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.dlha.addictinggame.R
+import com.dlha.addictinggame.adapter.CartAdapter
+import com.dlha.addictinggame.databinding.FragmentCartBinding
+import com.dlha.addictinggame.databinding.FragmentForumsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ForumsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ForumsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding : FragmentForumsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forums, container, false)
-    }
+        changeStatusBar()
+        binding = FragmentForumsBinding.inflate(layoutInflater)
+        setupToolbar()
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ForumsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ForumsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        return binding.root
+    }
+    private fun changeStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window: Window = requireActivity().window
+            window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+    }
+    private fun setupToolbar() {
+        (activity as AppCompatActivity?)!!.setSupportActionBar(binding.forumsToolbar)
+        (activity as AppCompatActivity?)!!.supportActionBar?.hide()
     }
 }
