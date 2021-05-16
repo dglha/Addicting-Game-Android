02902f96625ef4924c938ecbe17e0336d780b100
Duong Le Ha <duongleha212001@gmail.com>
2021-05-16 17:50:35 +0700

    ReviewsActivity.kt - Layout and Core, BottomSheet files.

--- app/src/main/java/com/dlha/addictinggame/ReviewsActivity.kt
+++ app/src/main/java/com/dlha/addictinggame/ReviewsActivity.kt
@@ -2,10 +2,25 @@ package com.dlha.addictinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dlha.addictinggame.databinding.ActivityReviewsBinding
 
 class ReviewsActivity : AppCompatActivity() {
+
+    private var _binding : ActivityReviewsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reviews)
+        _binding = ActivityReviewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.reviewToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        binding.reviewOpenBottomSheetFab.setOnClickListener {
+            val bottomSheet = ReviewBottomSheet()
+            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+        }
     }
 }
