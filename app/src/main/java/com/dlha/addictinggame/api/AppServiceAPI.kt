de422aa990171a504e52605c6970e11b9b61f57d
Duong Le Ha <duongleha212001@gmail.com>
2021-05-18 13:15:13 +0700

    Add method

--- app/src/main/java/com/dlha/addictinggame/api/AppServiceAPI.kt
+++ app/src/main/java/com/dlha/addictinggame/api/AppServiceAPI.kt
@@ -1,11 +1,16 @@
 package com.dlha.addictinggame.api
 
 import com.dlha.addictinggame.model.Category
-import retrofit2.Call
+import com.dlha.addictinggame.model.GameItem
+import com.dlha.addictinggame.model.Games
 import retrofit2.Response
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AppServiceAPI {
     @POST("api/listdanhmuc")
     suspend fun getListCategories() : Response<Category>
+
+    @GET("api/listNewGame")
+    suspend fun getListNewGames(): Response<List<GameItem>>
 }
