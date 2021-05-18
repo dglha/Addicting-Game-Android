44622d65330932ecf98b0f8c21cbc69c1fc7918c
Duong Le Ha <duongleha212001@gmail.com>
2021-05-18 11:05:56 +0700

    Removed login method.

--- app/src/main/java/com/dlha/addictinggame/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/dlha/addictinggame/viewmodels/MainViewModel.kt
@@ -20,36 +20,5 @@ class MainViewModel @Inject constructor(
     application: Application
 ): AndroidViewModel(application) {
 
-    /* Retrofit */
-    val userLoginResponse: MutableLiveData<NetworkResult<User>> = MutableLiveData()
 
-    fun userLogin(username: String, password: String) = viewModelScope.launch {
-        getUserLoginSafeCall(username, password)
-    }
-
-    private suspend fun getUserLoginSafeCall(username: String, password: String) {
-        userLoginResponse.value = NetworkResult.Loading()
-        try {
-            val response = repository.remote.userLogin(username, password)
-            userLoginResponse.value = handleUserLoginResponse(response)
-        } catch (e: Exception){
-            userLoginResponse.value = NetworkResult.Error("User not found")
-        }
-    }
-
-    private fun handleUserLoginResponse(response: Response<User>): NetworkResult<User> {
-        return when{
-            response.message().toString().contains("timeout") -> {
-                NetworkResult.Error("Timeout.")
-            }
-            response.body()!!.token.isEmpty() -> {
-                NetworkResult.Error("User not found!")
-            }
-            response.isSuccessful -> {
-                val user = response.body()
-                NetworkResult.Success(user!!)
-            }
-            else -> NetworkResult.Error(response.message().toString())
-        }
-    }
 }
