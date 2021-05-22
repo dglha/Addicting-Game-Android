d3ebe7f7b74d584b56a8859abe37648da99e375f
Duong Le Ha <duongleha212001@gmail.com>
2021-05-22 16:37:30 +0700

    Fix conflict

--- app/src/main/java/com/dlha/addictinggame/api/UserServiceAPI.kt
+++ app/src/main/java/com/dlha/addictinggame/api/UserServiceAPI.kt
@@ -2,7 +2,6 @@ package com.dlha.addictinggame.api
 
 import com.dlha.addictinggame.model.GameItem
 import com.dlha.addictinggame.model.Message
-import com.dlha.addictinggame.model.Message
 import com.dlha.addictinggame.model.UserResponse
 import retrofit2.Response
 import retrofit2.http.Field
