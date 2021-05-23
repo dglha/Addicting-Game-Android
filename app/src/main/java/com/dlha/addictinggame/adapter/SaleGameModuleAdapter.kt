be0f13d4a9c41a87c1133ae2960fd38dc046ec3b
Duong Le Ha <duongleha212001@gmail.com>
2021-05-23 20:48:42 +0700

    Change return list.size

--- app/src/main/java/com/dlha/addictinggame/adapter/SaleGameModuleAdapter.kt
+++ app/src/main/java/com/dlha/addictinggame/adapter/SaleGameModuleAdapter.kt
@@ -17,7 +17,6 @@ import coil.load
 import com.dlha.addictinggame.R
 import com.dlha.addictinggame.model.GameItem
 import com.dlha.addictinggame.ui.activities.DetailsActivity
-import com.dlha.addictinggame.ui.activities.SaleActivity
 import com.dlha.addictinggame.ui.fragments.home.HomeFragmentDirections
 import kotlin.math.roundToInt
 
@@ -67,7 +66,7 @@ class SaleGameModuleAdapter(val context : Context) : RecyclerView.Adapter<SaleGa
     }
 
     override fun getItemCount(): Int {
-        return 5
+        return if (games.size > 5) 5 else games.size
     }
 
     override fun onBindViewHolder(holder: SaleGameModuleAdapter.SaleGameModuleViewHolder, position: Int) {
